38ca21b5abed01838a7eefea17155b54596bff90
Freud233 <heima@88.com>
2023-04-20 23:02:21 +0800

    添加arcoUI

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import ArcoVue from '@arco-design/web-vue';
+import '@arco-design/web-vue/dist/arco.css';
 
-createApp(App).use(store).use(router).mount('#app')
+// const response = await openai.listEngines();
+createApp(App).use(store).use(router).use(ArcoVue).mount('#app');
